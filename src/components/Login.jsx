e512ff677598ddf5fc5ac8ffea69a67aae1365da
EdwinTorradoArt15 <torradoart15@gmail.com>
2022-09-26 08:47:48 -0500

    ruta categoria

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
               <label className='text-2xl'>Contraseña</label>
               <input placeholder='Contraseña' type="text" className='border relative bg-gray-100 p-2 focus:bg-white focus:outline-none' />
           </div>
-          <Link to={'/categoria'} className='w-full text-2xl block border-2 border-water-blue text-center bg-water-blue hover:bg-white hover:text-water-blue text-white font-semibold rounded-lg px-4 py-3 mt-6'>Iniciar sesion</Link>
+          <Link to={'/Vive-Colegio/categoria'} className='w-full text-2xl block border-2 border-water-blue text-center bg-water-blue hover:bg-white hover:text-water-blue text-white font-semibold rounded-lg px-4 py-3 mt-6'>Iniciar sesion</Link>
         </form>
       </div>
     </div>
